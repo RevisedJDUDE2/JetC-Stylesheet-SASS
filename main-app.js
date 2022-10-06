c8d5f4743667b4b2a2b1d72b3f196ff74e78f651
RevisedJDUDE2 <JDUDEofficial134@gmail.com>
2022-10-06 16:00:38 +0800

    Java Script ADDED!

---
+++ main-app.js
@@ -0,0 +1,4 @@
+const sya = alert("Jet C Styles Update List\n\nVersion 0.0.2 Brings You\n-Frame Updates\n-Bug Fixes\n-Java Script System\n-And Many More\n\nUpcoming Changes To Websites\n-Visitor Counter\n-Java Script Variable Counter\n-Much More\n\n\n@www.jetcstyle.cf Of October 6 2022")
+document.body.onload = () => {
+  sya()
+}
